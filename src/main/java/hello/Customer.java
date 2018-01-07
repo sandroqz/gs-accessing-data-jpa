5be6324fa2a8f7effd659ea0f798c2dcda200d40
Sandro Queiroz Jr <sandroqz@gmail.com>
2018-01-07 03:52:52 -0300

    Adds getter methods do Customer JPA entity

--- src/main/java/hello/Customer.java
+++ src/main/java/hello/Customer.java
@@ -22,6 +22,18 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
 	@Override
 	public String toString() {
 		return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
